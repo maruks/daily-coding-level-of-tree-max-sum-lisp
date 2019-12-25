dd8d03f27cee2dd89b07d776c4080341217ff384
Maris Orbidans <maris.orbidans@onthemarket.com>
2019-12-25 20:11:05 +0000

    use iterate

--- src/level-of-tree-max-sum.lisp
+++ src/level-of-tree-max-sum.lisp
@@ -4,13 +4,7 @@
 
 (in-package :level-of-tree-max-sum)
 
-
 (defstruct (node (:conc-name nil)) value left right)
-
-(defparameter *tree* (make-node :value 1
-				:left (make-node :value 4 :left (make-node :value 3) :right (make-node :value 2))
-				:right (make-node :value 5 :left (make-node :value 4) :right (make-node :value -1))))
-
 
 (defun level-values (root &optional (level 0))
   (when root
@@ -22,11 +16,9 @@
 (defun level-of-max-sum (tree)
   (let* ((lvals (level-values tree))
 	 (lsums (reduce (lambda (a e)
-			  (acons (car e) (+ (cdr e) (or (cdr (assoc (car e) a :test #'eql)) 0)) a)) lvals :initial-value nil)))
-    (loop :for i :upfrom 0
-	  :for lv = (assoc i lsums :test #'eql)
-	  :with max = (cons nil nil)
-	  :while lv
-	  :when (or (null (cdr max)) (< (cdr max) (cdr lv)))
-	    :do (setf max lv)
-	  :finally (return (car max)))))
+			  (acons (car e) (+ (cdr e) (or (cdr (assoc (car e) a)) 0)) a)) lvals :initial-value nil)))
+    (iterate
+      (for i :upfrom 0)
+      (for level-value = (assoc i lsums))
+      (while level-value)
+      (finding (car level-value) :maximizing (cdr level-value)))))
